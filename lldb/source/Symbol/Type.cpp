62d2c8a58f752845cb9eba438ce1fd57827b0845
Tom Tromey <tom@tromey.com>
2018-06-25 12:16:12 -0600

    Add a missing TypeAndOrName constructor
    Add a TypeAndOrName constructor that was declared but not defined.
    This is used in the Rust plugin.  See https://reviews.llvm.org/D44752

--- lldb/source/Symbol/Type.cpp
+++ lldb/source/Symbol/Type.cpp
@@ -694,6 +694,13 @@ TypeAndOrName::TypeAndOrName(const TypeAndOrName &rhs)
 TypeAndOrName::TypeAndOrName(ConstString &in_type_const_string)
     : m_type_name(in_type_const_string) {}
 
+TypeAndOrName::TypeAndOrName(const CompilerType &type)
+    : m_type_pair(type)
+{
+  if (m_type_pair)
+    m_type_name = m_type_pair.GetName();
+}
+
 TypeAndOrName &TypeAndOrName::operator=(const TypeAndOrName &rhs) {
   if (this != &rhs) {
     m_type_name = rhs.m_type_name;
