e13f47ffecced9bf9782a3cb1b557dfaea599dbf
Min-Yih Hsu <minyihh@uci.edu>
2021-07-18 14:18:46 -0700

    [MCA] Add MD_LAST to mark the last MD category

--- llvm/include/llvm/MCA/MetadataCategories.h
+++ llvm/include/llvm/MCA/MetadataCategories.h
@@ -4,6 +4,9 @@ namespace llvm {
 namespace mca {
 // Metadata for LSUnit
 static constexpr unsigned MD_LSUnit_MemAccess = 0;
+
+// Used for marking the start of custom MD Category
+static constexpr unsigned MD_LAST = MD_LSUnit_MemAccess;
 } // end namespace mca
 } // end namespace llvm
 #endif
